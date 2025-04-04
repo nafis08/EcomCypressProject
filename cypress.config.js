650ddc8f58d36012aaa17eec4948edbd426b9ceb
Zarin Tasnim <zarintasnim@macbookpro.speedport.ip>
2025-04-04 20:33:15 +0200

    Adding reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     specPattern: 'cypress/integration/examples/*.js',
 
@@ -12,6 +15,8 @@ module.exports = defineConfig({
   env: {
     url: "https://rahulshettyacademy.com"
   },
+
+  video: true,
 
 
 });
